b644e73ad01fe0f97c1c4f4234ab513f9dd72f28
shuvo151dey <shuvo151dey@gmail.com>
2020-10-19 16:16:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
 				<div className="name">Devank Thawre</div>
 				<div className="college">IIT KHARAGPUR</div>
 				<div className="details"> MANUFACTURING SCIENCE AND ENGINEERING (B.TECH)</div>
-				<div className="details"> Dept. Of MECHANICAL ENGG.</div>
+				<div className="details"> Dept. of MECHANICAL ENGG.</div>
 				<div className="details"> Expected April 2022</div>
 				<img className="header_image-1" src={require('./Assets/pc1.jpg')} alt="image1" />
 				<img className="header_image-2" src={require('./Assets/pc7.jpg')} alt="image2" />
-				<video autoPlay loop>
+				<video autoPlay loop muted>
 					<source src={require('./Assets/Tap.mp4')} type="video/mp4" />
 					Browser does not support mp4
 				</video>
@@ -58,7 +58,7 @@ function App() {
 					</div>
 				</div>
 				<h3>EXPERIENCE</h3>
-				<div>
+				<div className="content-box">
 					<h4>
 						<span>RESEARCH INTERN</span> | <span>ROBEL, CALFORNIA, USA</span>
 					</h4>
@@ -71,9 +71,12 @@ function App() {
 						simulate and study its dexterity by handling various tools.
 					</p>
 				</div>
-				<div>
+				<div className="content-box">
 					<h4>
-						<span>TeamKART Formula SAE</span> | <span>POWERTRAIN TEAM MEMBER</span>
+						<span>
+							<a href="https://teamkart.org/">TeamKART</a> Formula SAE
+						</span>{' '}
+						| <span>POWERTRAIN TEAM MEMBER</span>
 					</h4>
 					<p>SUPERVISOR: Dr Dhananjay Kumar Srivastava | Mechanical Engineering Department</p>
 					<p>September 2018 – January 2020 | IIT Kharagpur</p>
@@ -86,7 +89,7 @@ function App() {
 					</p>
 				</div>
 				<h3>PROJECTS</h3>
-				<div>
+				<div className="content-box">
 					<h4>
 						<span>AUTOMATED BATTERY CHANGING BOT</span>
 					</h4>
@@ -99,7 +102,7 @@ function App() {
 						of the bot for perfect execution of the required tasks usingArduino.
 					</p>
 				</div>
-				<div>
+				<div className="content-box">
 					<h4>
 						<span>AUXILIA</span> | <span>Brain Controlled Prosthetic Arm</span>
 					</h4>
@@ -111,7 +114,10 @@ function App() {
 						using 4 bar linkage mechanism for each joint in order to replicate natural movement of human
 						fingers.Performed stress analysis on the hand for strength optimization within material’s
 						limits.Developed controls of the hand using Arduino in order to orient the entire hand properly
-						for perfect gripping.
+						for perfect gripping.<a href="https://drive.google.com/file/d/1azUyxqiLfZOxdx6TCqba0VwjxyJ_DV2U/view?usp=sharing">
+							Link
+						</a>{' '}
+						to project.
 					</p>
 				</div>
 			</div>
